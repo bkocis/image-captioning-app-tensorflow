adb0712eecc48f3708757fd509fad40385bcb798
Balazs.Kocsis <balaz.kocis@gmail.com>
2023-02-08 23:06:45 +0100

    working on the trainer

--- training/trainer.py
+++ training/trainer.py
@@ -183,6 +183,58 @@ class TrainerClass:
 
         return train_raw, test_raw
 
+    def save_dataset(self, ds, save_path, image_model, tokenizer, shards=10, batch_size=32):
+        # Load the images and make batches.
+        ds = (ds
+              .map(lambda path, caption: (self.load_image(path), caption))
+              .apply(tf.data.experimental.ignore_errors())
+              .batch(batch_size))
+
+        # Run the feature extractor on each batch
+        # Don't do this in a .map, because tf.data runs on the CPU.
+        def gen():
+            for (images, captions) in tqdm.tqdm(ds):
+                feature_maps = image_model(images)
+
+                feature_maps, captions = self.match_shapes(feature_maps, captions)
+                yield feature_maps, captions
+
+        # Wrap the generator in a new tf.data.Dataset.
+        new_ds = tf.data.Dataset.from_generator(
+            gen,
+            output_signature=(
+                tf.TensorSpec(shape=image_model.output_shape),
+                tf.TensorSpec(shape=(None,), dtype=tf.string)))
+
+        # Apply the tokenization
+        new_ds = (new_ds
+                  .map(self.prepare_txt, tf.data.AUTOTUNE)
+                  .unbatch()
+                  .shuffle(1000))
+
+        # Save the dataset into shard files.
+        def shard_func(i, item):
+            return i % shards
+
+        new_ds.enumerate().save(save_path, shard_func=shard_func)
+
+    def load_dataset(self, save_path, batch_size=32, shuffle=1000, cycle_length=2):
+        def custom_reader_func(datasets):
+            datasets = datasets.shuffle(1000)
+            return datasets.interleave(lambda x: x, cycle_length=cycle_length)
+
+        ds = tf.data.Dataset.load(save_path, reader_func=custom_reader_func)
+
+        def drop_index(i, x):
+            return x
+
+        ds = (ds
+              .map(drop_index, tf.data.AUTOTUNE)
+              .shuffle(shuffle)
+              .padded_batch(batch_size)
+              .prefetch(tf.data.AUTOTUNE))
+        return ds
+
 
 if __name__ == '__main__':
     trainer = TrainerClass()
@@ -223,3 +275,9 @@ if __name__ == '__main__':
 
     train_ds = trainer.prepare_dataset(train_raw, tokenizer)
     test_ds = trainer.prepare_dataset(test_raw, tokenizer)
+
+    trainer.save_dataset(train_raw, 'train_cache', mobilenet, tokenizer)
+    trainer.save_dataset(test_raw, 'test_cache', mobilenet, tokenizer)
+
+    train_ds = trainer.load_dataset('train_cache')
+    test_ds = trainer.load_dataset('test_cache')
