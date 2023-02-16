7bf6dedf88b6eed983d51c4c26892f8f36f0435c
Balazs.Kocsis <balaz.kocis@gmail.com>
2023-02-16 23:07:27 +0100

    decoder layer

--- training/trainer.py
+++ training/trainer.py
@@ -27,6 +27,106 @@ import tensorflow_text as text
 import tensorflow_datasets as tfds
 
 
+class SeqEmbedding(tf.keras.layers.Layer):
+    def __init__(self, vocab_size, max_length, depth):
+        super().__init__()
+        self.pos_embedding = tf.keras.layers.Embedding(input_dim=max_length, output_dim=depth)
+
+        self.token_embedding = tf.keras.layers.Embedding(
+            input_dim=vocab_size,
+            output_dim=depth,
+            mask_zero=True)
+
+        self.add = tf.keras.layers.Add()
+
+    def call(self, seq):
+        seq = self.token_embedding(seq)  # (batch, seq, depth)
+
+        x = tf.range(tf.shape(seq)[1])  # (seq)
+        x = x[tf.newaxis, :]  # (1, seq)
+        x = self.pos_embedding(x)  # (1, seq, depth)
+
+        return self.add([seq, x])
+
+
+class CausalSelfAttention(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        # Use Add instead of + so the keras mask propagates through.
+        self.add = tf.keras.layers.Add()
+        self.layernorm = tf.keras.layers.LayerNormalization()
+
+    def call(self, x):
+        attn = self.mha(query=x, value=x,
+                        use_causal_mask=True)
+        x = self.add([x, attn])
+        return self.layernorm(x)
+
+
+class CrossAttention(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        self.add = tf.keras.layers.Add()
+        self.layernorm = tf.keras.layers.LayerNormalization()
+
+    def call(self, x, y, **kwargs):
+        attn, attention_scores = self.mha(
+            query=x, value=y,
+            return_attention_scores=True)
+
+        self.last_attention_scores = attention_scores
+
+        x = self.add([x, attn])
+        return self.layernorm(x)
+
+
+class FeedForward(tf.keras.layers.Layer):
+    def __init__(self, units, dropout_rate=0.1):
+        super().__init__()
+        self.seq = tf.keras.Sequential([
+            tf.keras.layers.Dense(units=2 * units, activation='relu'),
+            tf.keras.layers.Dense(units=units),
+            tf.keras.layers.Dropout(rate=dropout_rate),
+        ])
+
+        self.layernorm = tf.keras.layers.LayerNormalization()
+
+    def call(self, x):
+        x = x + self.seq(x)
+        return self.layernorm(x)
+
+
+class DecoderLayer(tf.keras.layers.Layer):
+    def __init__(self, units, num_heads=1, dropout_rate=0.1):
+        super().__init__()
+
+        self.self_attention = CausalSelfAttention(num_heads=num_heads,
+                                                  key_dim=units,
+                                                  dropout=dropout_rate)
+        self.cross_attention = CrossAttention(num_heads=num_heads,
+                                              key_dim=units,
+                                              dropout=dropout_rate)
+        self.ff = FeedForward(units=units, dropout_rate=dropout_rate)
+
+    def call(self, inputs, training=False):
+        in_seq, out_seq = inputs
+
+        # Text input
+        out_seq = self.self_attention(out_seq)
+
+        out_seq = self.cross_attention(out_seq, in_seq)
+
+        self.last_attention_scores = self.cross_attention.last_attention_scores
+
+        out_seq = self.ff(out_seq)
+
+        return out_seq
+
+
+#####
+
 class TrainerClass:
 
     def load_image(self, image_path):
